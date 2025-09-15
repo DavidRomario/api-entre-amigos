caaaff738d57cf21d4f0579273e1c7441291928a
“David <romario.david1997@gmail.com>
2025-09-15 14:25:55 -0300

    update 1

--- config/database.js
+++ config/database.js
@@ -2,10 +2,11 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const MAX_RETRIES = 10;
-const RETRY_DELAY = 3000; // 3 segundos
+const RETRY_DELAY = 3000;
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,

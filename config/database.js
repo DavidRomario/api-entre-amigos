6d973bb0553d15d8f545209e85886844238d3e40
“David <romario.david1997@gmail.com>
2025-09-15 14:09:22 -0300

    update

--- config/database.js
+++ config/database.js
@@ -3,12 +3,11 @@ require("dotenv").config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
-  connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT) || 10,
+  connectionLimit: Number(process.env.DB_CONNECTION_LIMIT) || 10,
 });
 
 (async () => {
